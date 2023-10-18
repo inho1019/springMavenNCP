4bffbf877664f43d9c0f70cc1bf5fec1f6100b6e
simin <inho1019@naver.com>
2023-10-18 14:15:54 +0900

    1

---
+++ src/main/webapp/WEB-INF/js/upload.js
@@ -0,0 +1,32 @@
+$(function(){
+	$('#uploadBtn').click(function(){
+		$('#imageNameDiv').empty()
+		$('#imageContentDiv').empty()
+		
+		if($('#imageName').val() == '') {
+			$('#imageNameDiv').text('상품명 압력');
+			$('#imageName').focus();
+		} else if($('#imageContent').val() == '') {
+			$('#imageContentDiv').text('상품 내용 압력');
+			$('#imageContent').focus();
+		} else {
+			var formData = new FormData($('#uploadForm')[0])
+			
+			$.ajax({
+				type: 'post',
+				enctype: 'multipart/form-data',
+				processData: false,
+				contentType: false,
+				url: '/springMavenNCP/user/upload',
+				data: formData,
+				success: function(data){
+					alert(data);
+					location.href='/springMavenNCP/user/uploadList'
+				},
+				error: function(e){
+					console.log(e)
+				}
+			})
+		}	
+	})
+})
